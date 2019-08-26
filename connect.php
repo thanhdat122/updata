e60a9e975128e832c406d9496d8d61932b713337
thanhdat122 <52903288+thanhdat122@users.noreply.github.com>
2019-08-26 17:38:27 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = pg_connect("host=ec2-174-129-27-158.compute-1.amazonaws.com port=5432 dbname=d692tlp6mgc3mc 
-user=qmiwpgmtylvvnq password=63251db4218016ad466a25a36fe6b2c76742cb871fa11da4a1051b7ebe4e3a71") 
+$conn = pg_connect("host=ec2-107-20-155-148.compute-1.amazonaws.com port=5432 dbname=d5ufu4523q420h 
+user=jwkrnprtpqjarr password=2179c242c2f73b05ce346e19d286eef8718b40f5827deedeaf5575c4ccecac6d") 
 or die("Connect Unsuccessful");
 ?>
