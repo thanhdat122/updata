08b62d7582bc833267cea00b43cdbd3f08b3b4c7
thanhdat122 <52903288+thanhdat122@users.noreply.github.com>
2019-09-01 17:35:32 +0700

    Update managing.php

--- managing.php
+++ managing.php
@@ -79,7 +79,21 @@ if(isset($_POST["aduser"]) && isset($_POST["adpass"]))
     background-color: #4CAF50;
     color: white;
     }
-
+    .buttonadd {
+      background-color: #f4511e;
+      border: none;
+      color: white;
+      padding: 16px 32px;
+      text-align: center;
+      font-size: 16px;
+      margin: 4px 2px;
+      opacity: 0.6;
+      transition: 0.3s;
+      display: inline-block;
+      text-decoration: none;
+      cursor: pointer;
+    }   
+    .buttonadd:hover {opacity: 1}
 </style>
 </head>
 
@@ -126,7 +140,7 @@ if(isset($_POST["aduser"]) && isset($_POST["adpass"]))
             }
             ?> 
         </table>
-        <button><a href="/add.php">Add More</a></button>
+        <button class="buttonadd"><a href="/add.php">Add More</a></button>
         <br><br>
     </div>
 </body>
