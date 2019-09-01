15a0ce4d1190f6d43311c5b2c488cdcb78d262f4
thanhdat122 <52903288+thanhdat122@users.noreply.github.com>
2019-09-01 13:25:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,36 +12,50 @@
     <title>Login</title>
 </head>
 <body>
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
-<div class="container">
-    <div class="card card-login mx-auto text-center bg-dark">
-        <div class="card-header mx-auto bg-dark">
-            <span> <img src="https://amar.vote/assets/img/amarVotebd.png" class="w-75" alt="Logo"> </span><br/>
-                        <span class="logo_title mt-5"> Login Dashboard </span>
-<!--            <h1>--><?php //echo $message?><!--</h1>-->
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
 
-        </div>
-        <div class="card-body">
-            <form action="" method="post">
-                <div class="input-group form-group">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text"><i class="fas fa-user"></i></span>
+<body>
+<div id="login">
+        <h3 class="text-center text-white pt-5">Login form</h3>
+        <div class="container">
+            <div id="login-row" class="row justify-content-center align-items-center">
+                <div id="login-column" class="col-md-6">
+                    <div id="login-box" class="col-md-12">
+                        <form id="login-form" class="form" action="managing.php" method="post">
+                            <h3 class="text-center text-info">Login</h3>
+                            <div class="form-group">
+                                <label for="username" class="text-info">Username:</label><br>
+                                <input type="text" name="username" id="username" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="password" class="text-info">Password:</label><br>
+                                <input type="text" name="password" id="password" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" name="submit" class="btn btn-info btn-md" value="submit">
+                                 <input type="submit" name="submit" class="btn btn-info btn-md" value="Login">
+                            </div>
+                           
+                        </form>
                     </div>
-                    <input type="text" name="email" class="form-control" placeholder="Username">
                 </div>
-
-                <div class="input-group form-group">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text"><i class="fas fa-key"></i></span>
-                    </div>
-                    <input type="password" name="password" class="form-control" placeholder="Password">
-                </div>
-
-                <div class="form-group">             
-                    <input type="submit" value="Login" class="btn btn-outline-danger float-right login_btn">
-                </div>
-            </form>
+            </div>
         </div>
     </div>
-</div>
+</body>
+
+<body class="login">
+    <div class="login">
+        <h6>Login to admin</h6>
+        <form class="box" action="managing.php" method="POST">
+            <input type="text" name="aduser" placeholder="Username"> <br>
+            <input type="password" name="adpass" placeholder="Password"> <br>
+            <input type="submit" value="Login">
+        </form>
+    </div>
+</body>
+
 </html>
