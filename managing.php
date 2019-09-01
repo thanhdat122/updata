28c7efaf9676420d10fa605a841c6c4f3e76e235
thanhdat122 <52903288+thanhdat122@users.noreply.github.com>
2019-09-01 17:24:27 +0700

    a

--- managing.php
+++ managing.php
@@ -32,27 +32,53 @@ if(isset($_POST["aduser"]) && isset($_POST["adpass"]))
     <title>Document</title>
     <style>
     #customers {
-  font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
-}
+    font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+    }
 
-#customers td, #customers th {
-  border: 1px solid #ddd;
-  padding: 8px;
-}
+    #customers td, #customers th {
+    border: 1px solid #ddd;
+    padding: 8px;
+    }
 
-#customers tr:nth-child(even){background-color: ;}
+    #customers tr:nth-child(even){background-color: ;}
 
-#customers tr:hover {background-color: #ddd;}
+    #customers tr:hover {background-color: #ddd;}
 
-#customers th {
-  padding-top: 12px;
-  padding-bottom: 12px;
-  text-align: left;
-  background-color: #4CAF50;
-  color: white;
-}
+    #customers th {
+    padding-top: 12px;
+    padding-bottom: 12px;
+    text-align: left;
+    background-color: #4CAF50;
+    color: white;
+    }
+    .button {
+    background-color: #4CAF50; /* Green */
+    border: none;
+    color: white;
+    padding: 16px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    -webkit-transition-duration: 0.4s; /* Safari */
+    transition-duration: 0.4s;
+    cursor: pointer;
+    }
+
+    .button1 {
+    background-color: white; 
+    color: black; 
+    border: 2px solid #4CAF50;
+    }
+
+    .button1:hover {
+    background-color: #4CAF50;
+    color: white;
+    }
+
 </style>
 </head>
 
@@ -83,21 +109,18 @@ if(isset($_POST["aduser"]) && isset($_POST["adpass"]))
                 <td>
                 <form action='/delete.php' method="POST">
                 <input type='hidden' name='productid' value='<?php echo $row['productid']?>'>
-                <input class="edit-btn" type='submit' value='Delete'>
+                <input class="button1" type='submit' value='Delete'>
                 </form> <br>
                 <form action="/update.php" method="POST">
                 <input type='hidden' name='productid' value='<?php echo $row['productid']?>'>
                 <input type='hidden' name='name' value='<?php echo $row['proname']?>'>
                 <input type='hidden' name='price' value='<?php echo $row['price']?>'>
                 <input type='hidden' name='descrip' value='<?php echo $row['descrip']?>'>
-                <input class="edit-btn" type='submit' value='Update'>
+                <input class="button1" type='submit' value='Update'>
                 </form>
                 </td>
 
             </tr>
-            <?php
-            }
-            ?> 
         </table>
         <button><a href="/add.php">Add More</a></button>
         <br><br>
