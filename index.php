47a28bd5efa869145aadfdf1bd192605891dd0b3
thanhdat122 <52903288+thanhdat122@users.noreply.github.com>
2019-09-01 13:18:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,18 +6,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/style.css">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <title>Login</title>
 </head>
+<body>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+<div class="container">
+    <div class="card card-login mx-auto text-center bg-dark">
+        <div class="card-header mx-auto bg-dark">
+            <span> <img src="https://amar.vote/assets/img/amarVotebd.png" class="w-75" alt="Logo"> </span><br/>
+                        <span class="logo_title mt-5"> Login Dashboard </span>
+<!--            <h1>--><?php //echo $message?><!--</h1>-->
 
-<body class="login">
-    <div class="login">
-        <h6>Login to admin</h6>
-        <form class="box" action="managing.php" method="POST">
-            <input type="text" name="aduser" placeholder="Username"> <br>
-            <input type="password" name="adpass" placeholder="Password"> <br>
-            <input type="submit" value="Login">
-        </form>
+        </div>
+        <div class="card-body">
+            <form action="" method="post">
+                <div class="input-group form-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text"><i class="fas fa-user"></i></span>
+                    </div>
+                    <input type="text" name="email" class="form-control" placeholder="Username">
+                </div>
+
+                <div class="input-group form-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text"><i class="fas fa-key"></i></span>
+                    </div>
+                    <input type="password" name="password" class="form-control" placeholder="Password">
+                </div>
+
+                <div class="form-group">             
+                    <input type="submit" value="Login" class="btn btn-outline-danger float-right login_btn">
+                </div>
+            </form>
+        </div>
     </div>
-</body>
-
+</div>
 </html>
