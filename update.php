45e14c1394224b15cbac4f86ee987934db6d2171
thanhdat122 <52903288+thanhdat122@users.noreply.github.com>
2019-09-02 10:32:28 +0700

    Update update.php

--- update.php
+++ update.php
@@ -87,7 +87,7 @@ if (isset($_POST['update'])) {
 			</div>
 
 		</form>
-                <button style="margin-left: 100px;"><a href="/managing.php">Back</a></button>
+                <button ><a href="/managing.php">Back</a></button>
 		<br><br>
     </div>
 </body>
