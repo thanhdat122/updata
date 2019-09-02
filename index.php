95b491c8abe5b4285d02d2d084bd4cc0be3793e6
thanhdat122 <52903288+thanhdat122@users.noreply.github.com>
2019-09-02 10:07:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -16,21 +16,23 @@
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <!------ Include the above in your HEAD tag ---------->
+<fr
+
 <div id="login">
         
         <div class="container">
             <div id="login-row" class="row justify-content-center align-items-center">
                 <div id="login-column" class="col-md-6">
                     <div id="login-box" class="col-md-12">
-                        <form id="login-form" class="form" action="managing.php" method="post">
+                        <form id="login-form" class="form" action="managing.php" method="POST">
                             <h3 class="text-center text-info">Login</h3>
                             <div class="form-group">
-                                <label for="username" class="text-info">Username:</label><br>
-                                <input type="text" name="username" id="username" class="form-control">
+                                <label for="username" class="text-info">Username:</label><br>     
+                                <input type="text" name="aduser" placeholder="Username" class="form-control"> <br>
                             </div>
                             <div class="form-group">
-                                <label for="password" class="text-info">Password:</label><br>
-                                <input type="password" name="password" id="password" class="form-control">
+                                <label for="password" class="text-info">Password:</label><br>   
+                        <input type="password" name="adpass" placeholder="Password"class="form-control"> <br>
                             </div>
                             <div class="form-group">
                                 <input type="submit" name="Submit" class="btn btn-info btn-md" value="Login">
